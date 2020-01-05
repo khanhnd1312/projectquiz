a81cda3353a5ad19beaf2b7537dd96b6c3cf7187
Khánh's MacbookPro <khanhminh9x@gmail.com>
2020-01-06 00:27:49 +0700

    update get request

--- src/main/java/com/example/projectquiz/model/Course.java
+++ src/main/java/com/example/projectquiz/model/Course.java
@@ -1,10 +1,17 @@
 package com.example.projectquiz.model;
 
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.sql.Time;
 
+@Getter
+@Setter
 @Entity(name="tbl_course")
 //@Table(name = "tbl_course")
 public class Course implements Serializable {
