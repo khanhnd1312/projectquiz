fdf051efd86860345723a01ffcda50fe1669956d
Khánh's MacbookPro <khanhminh9x@gmail.com>
2020-01-07 14:46:05 +0700

    update

--- src/test/java/com/example/projectquiz/postapi/PostNewUserTest.java
+++ src/test/java/com/example/projectquiz/postapi/PostNewUserTest.java
@@ -13,54 +13,56 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class PostNewUserTest {
 
-//    @BeforeAll
-//    static void init() {
-//        RestAssured.baseURI = "http://localhost";
-//        //RestAssured.basePath = "/createuser";
-//        RestAssured.port = 8080;
-//    }
-//
-//    @Test
-//    public void postNewStudent() {
-//        User user = new User();
-//        user.setIdUser(22);
-//        user.setAccountUser("accountTest");
-//        user.setPasswordUser("passtest");
-//
-//        String testBody = "{    \"idUser\": 22,    \"accountUser\": \"democreateuser\",    \"passwordUser\": \"demopass2\"}";
-//
-//        Response res = given()
-//                .contentType(ContentType.JSON)
-//                .body(testBody)
-//                .when()
-//                .post("/createuser");
-//        //res.prettyPrint();
-//        res.then().statusCode(201).body("msg ", equalTo("User added"));
-//    }
-@Test
-public void createEmployee() {
-
-    RestAssured.baseURI = "localhost:8080";
-
-    String testBody = "{    \"idUser\": 22,    \"accountUser\": \"democreateuser\",    \"passwordUser\": \"demopass2\"}";
-
-
-    Response response = null;
-
-    try {
-        response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(testBody)
-                .post("/createuser");
-    } catch (Exception e) {
-        e.printStackTrace();
+    @BeforeAll
+    static void init() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.basePath = "/createuser";
+        RestAssured.port = 8080;
     }
 
-//    System.out.println("Response :" + response.asString());
-//    System.out.println("Status Code :" + response.getStatusCode());
-//    System.out.println("Does Reponse contains 'tammy'? :" + response.asString().contains("tammy"));
+    @Test
+    public void postNewStudent() {
+        User user = new User();
+        user.setIdUser(22);
+        user.setAccountUser("accountTest");
+        user.setPasswordUser("passtest");
+
+        //String testBody = "{    \"idUser\": 22,    \"accountUser\": \"democreateuser\",    \"passwordUser\": \"demopass2\"}";
+
+        Response res = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post();
+        //res.prettyPrint();
+        res.then().statusCode(201).body("msg ", equalTo("User added"));
+    }
 
 
-    Assert.assertEquals(200, response.getStatusCode());
-}
+//    @Test
+//    public void createEmployee() {
+//
+//    RestAssured.baseURI = "localhost:8080";
+//
+//    String testBody = "{    \"idUser\": 22,    \"accountUser\": \"democreateuser\",    \"passwordUser\": \"demopass2\"}";
+//
+//
+//    Response response = null;
+//
+//    try {
+//        response = RestAssured.given()
+//                .contentType(ContentType.JSON)
+//                .body(testBody)
+//                .post("/createuser");
+//    } catch (Exception e) {
+//        e.printStackTrace();
+//    }
+//
+////    System.out.println("Response :" + response.asString());
+////    System.out.println("Status Code :" + response.getStatusCode());
+////    System.out.println("Does Reponse contains 'tammy'? :" + response.asString().contains("tammy"));
+//
+//
+//    Assert.assertEquals(200, response.getStatusCode());
+//    }
 }
