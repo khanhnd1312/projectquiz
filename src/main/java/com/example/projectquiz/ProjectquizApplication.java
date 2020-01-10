3abed2fdcb9d2307f12ede8ec4086725a6fd5f81
Khánh's MacbookPro <khanhminh9x@gmail.com>
2020-01-11 00:43:38 +0700

    fix cors errror

--- src/main/java/com/example/projectquiz/ProjectquizApplication.java
+++ src/main/java/com/example/projectquiz/ProjectquizApplication.java
@@ -2,14 +2,28 @@ package com.example.projectquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProjectquizApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectquizApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "DELETE");
+            }
+        };
     }
 
 }
