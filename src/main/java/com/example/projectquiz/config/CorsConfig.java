6db9c570f7626f175d5b5cef835b88ff8916733a
william9x <lehoangnam197@gmail.com>
2020-01-11 12:22:14 +0700

    allow all methods in cors config

--- src/main/java/com/example/projectquiz/config/CorsConfig.java
+++ src/main/java/com/example/projectquiz/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**");
+        registry.addMapping("/api/**").allowedHeaders("GET", "POST", "PUT", "DELETE");
     }
 
 }
